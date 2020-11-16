39651b7c8829c5407a719f393e30ff77ddf0062b
Mahfoudh Kerbouche <mahfoudh.kerbouche@etu.univ-lyon1.fr>
2020-11-16 16:18:38 +0100

    Ajout des servlets d'inscription et de login pour les utilisateurs et les clubs

---
+++ sportify/src/main/java/fr/univ/lyon1/m1if/m1if10Grp13/servlets/ClubInscription.java
@@ -0,0 +1,39 @@
+package fr.univ.lyon1.m1if.m1if10Grp13.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ClubInscription
+ */
+public class ClubInscription extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ClubInscription() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
