4d29d5918d0065c880a2347ef2a3d4e99cb4169e
p2020623 Mahfoudh KERBOUCHE <mahfoudh.kerbouche@etu.univ-lyon1.fr>
2020-11-25 00:38:41 +0100

    Modification finale du code d'affichage de planning

--- sportify/src/main/java/fr/univ/lyon1/m1if/m1if10Grp13/dao/DAOReservationTerrain.java
+++ sportify/src/main/java/fr/univ/lyon1/m1if/m1if10Grp13/dao/DAOReservationTerrain.java
@@ -19,14 +19,11 @@ import fr.univ.lyon1.m1if.m1if10Grp13.classes.Inscrit;
 import fr.univ.lyon1.m1if.m1if10Grp13.classes.ReservationTerrain;
 import fr.univ.lyon1.m1if.m1if10Grp13.daoException.DAOException;
 
+public class DAOReservationTerrain implements DAOCrud {
 
-public class DAOReservationTerrain implements DAOCrud{
-	
 	// Injection du manager, qui s'occupe de la connexion avec la BDD
-	@PersistenceContext( unitName = "pu-sportify" )
-	private EntityManagerFactory factory
-	;
-
+	@PersistenceContext(unitName = "pu-sportify")
+	private EntityManagerFactory factory;
 
 	public DAOReservationTerrain(EntityManagerFactory factory) {
 		this.factory = factory;
@@ -42,150 +39,150 @@ public class DAOReservationTerrain implements DAOCrud{
 			reservation = (ReservationTerrain) objet;
 			System.out.println(reservation);
 		}
-		
+
 		try {
 //			entitymanager.persist(reservation);
-        	// Lancement d'une transaction
-        	entitymanager.getTransaction( ).begin( );
-        	System.out.println("Before reservation");
-        	// Modification de la table
-			entitymanager.merge( reservation );
-			///entitymanager.persist(reservation);
+			// Lancement d'une transaction
+			entitymanager.getTransaction().begin();
+			System.out.println("Before reservation");
+			// Modification de la table
+			entitymanager.merge(reservation);
+			/// entitymanager.persist(reservation);
 			System.out.println("After reservation");
 
-            // Mise à jours de la table
-            entitymanager.getTransaction().commit();
+			// Mise à jours de la table
+			entitymanager.getTransaction().commit();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        } finally {
-            // Femeture de l'objet ntityManager
-            entitymanager.close();
-        }
-        return false;
-        // TODO Auto-generated method stub
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			// Femeture de l'objet ntityManager
+			entitymanager.close();
+		}
+		return false;
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public Object afficher(Object object) throws DAOException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public Object afficher(Object object) throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public void update(Object object, Object id) throws DAOException {
-        // TODO Auto-generated method
+	@Override
+	public void update(Object object, Object id) throws DAOException {
+		// TODO Auto-generated method
 
-    }
+	}
 
-    @Override
-    public int delete(Object object) throws DAOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	@Override
+	public int delete(Object object) throws DAOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    /**
-     * retourn la liste de toutes les reservation dans la table reservation
-     * 
-     * @return Liste d'objet ReservationTerrain
-     */
-    @SuppressWarnings("unchecked")
-    public List<ReservationTerrain> afficherAll() {
-        EntityManager entitymanager = this.factory.createEntityManager();
-        List<ReservationTerrain> listReservation = null;
-        String query = "SELECT r FROM ReservationTerrain r";
-        try {
-            listReservation = entitymanager.createQuery(query).getResultList();
-            for (ReservationTerrain reservation : listReservation) {
-                System.out.println(reservation.getClub().getNomClub());
-            }
-        } catch (DAOException e) {
-            e.printStackTrace();
-        }
-        return listReservation;
+	/**
+	 * retourn la liste de toutes les reservation dans la table reservation
+	 * 
+	 * @return Liste d'objet ReservationTerrain
+	 */
+	@SuppressWarnings("unchecked")
+	public List<ReservationTerrain> afficherAll() {
+		EntityManager entitymanager = this.factory.createEntityManager();
+		List<ReservationTerrain> listReservation = null;
+		String query = "SELECT r FROM ReservationTerrain r";
+		try {
+			listReservation = entitymanager.createQuery(query).getResultList();
+			for (ReservationTerrain reservation : listReservation) {
+				System.out.println(reservation.getClub().getNomClub());
+			}
+		} catch (DAOException e) {
+			e.printStackTrace();
+		}
+		return listReservation;
 
-    }
+	}
 
-    /**
-     * Verifier si un creneau donné est reservé ou pas.
-     * 
-     * @param heure chaine de caractère qui représente l'heure de debut
-     * @param nJour le numéro du jour de la semaine actuelle pour lequel on veut les
-     *              dispos.
-     * @param user  instance de la classe inscrit si l'utilisateur connecté est un
-     *              adherent, club sinon.
-     * @return "reserve" si le creneau est reservé par la personne donnée en
-     *         parametre, "disponible" si le creneau est disponible "occupe" si le
-     *         creneau est reservé par une autre personne
-     */
-    public String creneauDispo(String heure, int nJour, Object user) {
-        EntityManager entitymanager = this.factory.createEntityManager();
-        ReservationTerrain reservation;
-        final Calendar date = new GregorianCalendar();
+	/**
+	 * Verifier si un creneau donné est reservé ou pas.
+	 * 
+	 * @param heure chaine de caractère qui représente l'heure de debut
+	 * @param nJour le numéro du jour de la semaine actuelle pour lequel on veut les
+	 *              dispos.
+	 * @param user  instance de la classe inscrit si l'utilisateur connecté est un
+	 *              adherent, club sinon.
+	 * @return "reserve" si le creneau est reservé par la personne donnée en
+	 *         parametre, "disponible" si le creneau est disponible "occupe" si le
+	 *         creneau est reservé par une autre personne
+	 */
+	public String creneauDispo(String heure, int nJour, Object user) {
+		EntityManager entitymanager = this.factory.createEntityManager();
+		ReservationTerrain reservation;
+		final Calendar date = new GregorianCalendar();
 
-        try {
-            while (date.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
-                date.add(Calendar.DAY_OF_WEEK, -1);
-            }
-            date.add(Calendar.DAY_OF_WEEK, nJour);
-            SimpleDateFormat fmtDate = new SimpleDateFormat("yyyy-MM-dd");
-            // System.out.println(fmtDate.format(date.getTime()));
-            Time heureCreneau = (Time) Time.valueOf(heure);
-            Date dateCreneau = (Date) Date.valueOf(fmtDate.format(date.getTime()));
-            String request = "SELECT r FROM ReservationTerrain r JOIN r.creneau c "
-                    + "WHERE c.datecreneau = :datecreneau AND c.heurecreneau =:heurecreneau";
-            reservation = (ReservationTerrain) entitymanager.createQuery(request)
-                    .setParameter("datecreneau", dateCreneau).setParameter("heurecreneau", heureCreneau)
-                    .getSingleResult();
-            if (user instanceof Inscrit) {
-                Inscrit utilisateur = (Inscrit) user;
-                if (reservation.getInscrit() != null
-                        && reservation.getInscrit().getEmailInscrit().equals(utilisateur.getEmailInscrit())) {
-                    return "reserve";
-                }
-            } else if (user instanceof Club) {
-                Club utilisateur = (Club) user;
-                if (reservation.getClub() != null
-                        && reservation.getClub().getEmailClub().equals(utilisateur.getEmailClub())) {
-                    return "reserve";
-                }
-            }
+		try {
+			while (date.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+				date.add(Calendar.DAY_OF_WEEK, -1);
+			}
+			date.add(Calendar.DAY_OF_WEEK, nJour);
+			SimpleDateFormat fmtDate = new SimpleDateFormat("yyyy-MM-dd");
+			// System.out.println(fmtDate.format(date.getTime()));
+			Time heureCreneau = (Time) Time.valueOf(heure);
+			Date dateCreneau = (Date) Date.valueOf(fmtDate.format(date.getTime()));
+			String request = "SELECT r FROM ReservationTerrain r JOIN r.creneau c "
+					+ "WHERE c.datecreneau = :datecreneau AND c.heurecreneau =:heurecreneau";
+			reservation = (ReservationTerrain) entitymanager.createQuery(request)
+					.setParameter("datecreneau", dateCreneau).setParameter("heurecreneau", heureCreneau)
+					.getSingleResult();
+			if (user instanceof Inscrit) {
+				Inscrit utilisateur = (Inscrit) user;
+				if (reservation.getInscrit() != null
+						&& reservation.getInscrit().getEmailInscrit().equals(utilisateur.getEmailInscrit())) {
+					return "reserve";
+				}
+			} else if (user instanceof Club) {
+				Club utilisateur = (Club) user;
+				if (reservation.getClub() != null
+						&& reservation.getClub().getEmailClub().equals(utilisateur.getEmailClub())) {
+					return "reserve";
+				}
+			}
 
-        } catch (NoResultException e) {
-            // System.out.println("No result found, creneau est dispo");
-            return "disponible";
-        }
-        return "occupe";
+		} catch (NoResultException e) {
+			// System.out.println("No result found, creneau est dispo");
+			return "disponible";
+		}
+		return "occupe";
 
-    }
+	}
 
-    /**
-     * Recuperer la liste des reservation d'une date données.
-     * 
-     * @param date La date cherchée.
-     * @return une liste contenant toutes les instances de la classe
-     *         ReservationTerrain correspondante à la date donnée, En ordre
-     *         ascendant
-     */
-    public ArrayList listReservationJours(String date) {
-        EntityManager entitymanager = this.factory.createEntityManager();
-        ArrayList<ReservationTerrain> listReservation = null;
-        Date datecreneau = Date.valueOf(date);
-        String query = "SELECT r FROM ReservationTerrain r JOIN r.creneau c WHERE c.datecreneau =: datecreneau"
-                + " ORDER BY c.heurecreneau ASC";
-        try {
-            listReservation = (ArrayList<ReservationTerrain>) entitymanager.createQuery(query)
-                    .setParameter("datecreneau", datecreneau).getResultList();
-        } catch (DAOException e) {
-            e.printStackTrace();
-        }
-        return listReservation;
+	/**
+	 * Recuperer la liste des reservation d'une date données.
+	 * 
+	 * @param date La date cherchée.
+	 * @return une liste contenant toutes les instances de la classe
+	 *         ReservationTerrain correspondante à la date donnée, En ordre
+	 *         ascendant
+	 */
+	public ArrayList listReservationJours(String date, Long terrainId) {
+		EntityManager entitymanager = this.factory.createEntityManager();
+		ArrayList<ReservationTerrain> listReservation = null;
+		Date datecreneau = Date.valueOf(date);
+		String query = "SELECT r FROM ReservationTerrain r JOIN r.creneau c JOIN r.terrain t  "
+				+ "WHERE c.datecreneau =: datecreneau AND t.terrainId =: terrainid" + " ORDER BY c.heurecreneau ASC";
+		try {
+			listReservation = (ArrayList<ReservationTerrain>) entitymanager.createQuery(query)
+					.setParameter("datecreneau", datecreneau).setParameter("terrainid", terrainId).getResultList();
+		} catch (DAOException e) {
+			e.printStackTrace();
+		}
+		return listReservation;
 
-    }
+	}
 
-    /**
+	/**
      * Recupérer la date de chaque jours de la semaine courante, puis effectuer une
      * recherche dans la BD En utilisant la méthode listReservationJours pour chaque
      * date afin de trouver les créneaux reservés.
@@ -193,7 +190,7 @@ public class DAOReservationTerrain implements DAOCrud{
      * @return une TreeMap contenat des pairs <Date, ListReservations> qui associe à
      *         chaque date la liste des créneaux reservés.
      */
-    public TreeMap getWeekReservations() {
+    public TreeMap getWeekReservations(Long terrainId) {
         // Convertir la format de date
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -213,7 +210,7 @@ public class DAOReservationTerrain implements DAOCrud{
 
         // Ajout de la date du jours courant au Hashmap
         String date = sdf.format(cal.getTime());
-        weekDatesMap.put(date, listReservationJours(date));
+        weekDatesMap.put(date, listReservationJours(date, terrainId));
 
         // Recupération des date de la semaine courante
         while (current - calIndex > 0 || current + calIndex < 8) {
@@ -221,14 +218,14 @@ public class DAOReservationTerrain implements DAOCrud{
             if (current - calIndex > 0) {
                 cal.add(Calendar.DATE, 1);
                 date = sdf.format(cal.getTime());
-                weekDatesMap.put(date, listReservationJours(date));
+                weekDatesMap.put(date, listReservationJours(date, terrainId));
             }
 
             // Récupération des dates des jours précédents
             if (current + calIndex < 8) {
                 cal2.add(Calendar.DATE, -1);
                 date = sdf.format(cal2.getTime());
-                weekDatesMap.put(date, listReservationJours(date));
+                weekDatesMap.put(date, listReservationJours(date, terrainId));
             }
             calIndex += 1;
         }
--- sportify/src/main/java/fr/univ/lyon1/m1if/m1if10Grp13/servlets/PlanningGestion.java
+++ sportify/src/main/java/fr/univ/lyon1/m1if/m1if10Grp13/servlets/PlanningGestion.java
@@ -1,11 +1,21 @@
 package fr.univ.lyon1.m1if.m1if10Grp13.servlets;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.univ.lyon1.m1if.m1if10Grp13.classes.ReservationTerrain;
+import fr.univ.lyon1.m1if.m1if10Grp13.dao.DAOReservationTerrain;
 
 /**
  * Servlet implementation class PlanningGestion
@@ -13,26 +23,51 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "PlanningGestion", urlPatterns = "/PlanningGestion")
 public class PlanningGestion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PlanningGestion() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private ServletContext servletContext;
+	private DAOReservationTerrain daoReservation;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public PlanningGestion() {
+
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+
+		this.servletContext = config.getServletContext();
+		this.daoReservation = (DAOReservationTerrain) this.servletContext.getAttribute("daoReservation");
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String stade = (String) request.getParameter("stade");
+		Long stadeId = Long.valueOf(stade);
+		TreeMap<String, ArrayList> planning = daoReservation.getWeekReservations(stadeId);
+		request.setAttribute("planning", planning);
+
+		for (Entry<String, ArrayList> entry : planning.entrySet()) {
+			String key = entry.getKey();
+			ArrayList<ReservationTerrain> value = entry.getValue();
+			for (ReservationTerrain r : value) {
+				System.out.println(new SimpleDateFormat("hh:mm:ss").format(r.getCreneauId().getHeureCreneau()));
+			}
+		}
+		request.getRequestDispatcher("interface.jsp").include(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// Not implemented because no post requests will be sent
 	}
 
